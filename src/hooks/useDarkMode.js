706cfacf0e8d87a2d33333dafe585d11a799d0c2
Andrew Ackerman <andrew.ackers@gmail.com>
2019-12-04 14:23:49 -0800

    Finished Project

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = (initialValue) => {
+    const [inputValue, setInputValue] = useLocalStorage(initialValue);
+    useEffect(() => {
+        const body = window.document.body;
+        return inputValue ? body.classList.add('dark-mode') : body.classList.remove('dark-mode')
+    }, [inputValue])
+    return [inputValue, setInputValue];
+}
+
+export default useDarkMode;
